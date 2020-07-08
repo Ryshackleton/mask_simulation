aac09c0d2d02b620a4fa1fd9efa21a68f64029d7
Ryan Shackleton <ryshack@uw.edu>
2020-07-08 15:57:56 -0700

    Flesh out the demo page a bit.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import { DEFAULT_SIMULATION_PROPS, DEFAULT_VIRUS_SIMULATION_PROPS } from './hook
 function App() {
   return (
     <div className="App">
+      <div className="section">
+        <h2 className="title">Basic probability based transmission model</h2>
+        <div className="editorial-text">This simulation demonstrates the concepts that mask use in the general population helps
+          flatten the curve by lowering the overall rate of virus transmission.  If we take a base
+          transmission rate of 5% probability of virus transmision in nodes that come in contact,
+          we see the following scenario.
+        </div>
+        <div className="editorial-text"><strong>Click/touch the gray box to start/stop/reset simulations</strong></div>
         <MaskSimulation simulationProps={{
           ...DEFAULT_SIMULATION_PROPS,
           virusSimulations: [
@@ -16,6 +24,32 @@ function App() {
             },
           ],
         }} />
+      </div>
+      <div className="section">
+        <h2 className="title">Masks vs no masks</h2>
+        <div className="editorial-text">Now lets run two simulations like the one above that have
+          the same contacts for each node and the same transmission probabilities, but now we'll
+          add masks to one simulation. <strong>Masked individuals are denoted by the black outlined
+          circles.</strong> Here we assume that the masks reduce the probability of virus
+          transmission by 33% for both the infected person and the healthy person. See how the model
+          changes.
+        </div>
+        <MaskSimulation simulationProps={{
+          ...DEFAULT_SIMULATION_PROPS,
+          virusSimulations: [
+            {
+              ...DEFAULT_VIRUS_SIMULATION_PROPS,
+              title: 'No mask use',
+              percentMasked: 0,
+            },
+            {
+              ...DEFAULT_VIRUS_SIMULATION_PROPS,
+              title: '95% mask use',
+              percentMasked: 95,
+            }
+          ],
+        }}/>
+      </div>
     </div>
   );
 }
