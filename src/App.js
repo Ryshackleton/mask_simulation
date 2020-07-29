a2ddadd862657be04da0f94ef4e049d6351dbd35
Ryan Shackleton <ryshack@uw.edu>
2020-07-28 22:50:31 -0400

    Revert unnecessary url mapping.

--- src/App.js
+++ src/App.js
@@ -13,14 +13,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        {['/mask_simulation/no_masks', '/no_masks'].map((path) => (
-          <Route
-            key="no_masks"
-            exact path={path}
-            render={() => <MaskSimulation simulationProps={NO_MASKS_PROPS} />}
-          />
-        ))}
-        <Route exact path="/mask_simulation/no_masks_vs_masks" >
+        <Route path="/mask_simulation/no_masks" >
+          <MaskSimulation simulationProps={NO_MASKS_PROPS} />
+        </Route>
+        <Route path="/mask_simulation/no_masks_vs_masks" >
           <MaskSimulation simulationProps={NO_MASKS_VS_MASKS_PROPS} />
         </Route>
         <Route path="/mask_simulation/no_masks_vs_masks_with_social_distancing" >
